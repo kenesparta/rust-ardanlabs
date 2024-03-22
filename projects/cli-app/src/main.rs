2f19c6cfb3ad76b0f95330356f6b08855e48588b
Ken Esparta <kenesparta@pm.me>
2024-03-21 23:46:24 -0500

    adding new Args parsing

--- projects/cli-app/src/main.rs
+++ projects/cli-app/src/main.rs
@@ -1,18 +1,31 @@
-use clap::{Command, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command]
+#[command()]
 struct Args {
     #[command(subcommand)]
-    command: Option<Command>
+    command: Option<LoginCommands>
 }
 
 #[derive(Subcommand)]
-enum Commands {
+enum LoginCommands {
     /// List all users
     List,
 }
 
+fn list_users() {
+    println!("{:<20}{:<20}", "Username", "Password");
+    println!("{:-<40}", "");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+    match cli.command {
+        Some(LoginCommands::List) => {
+            print!("List Users Here");
+        }
+        None => {
+            println!("Run with --help to see instructions.")
+        }
+    }
 }
