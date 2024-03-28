dc3207a5ea375a8ac897e219a0dbbf9050b0a5c1
Ken Esparta <kenesparta@pm.me>
2024-03-27 22:40:34 -0500

    async threads using a vector

--- async-mt/src/main.rs
+++ async-mt/src/main.rs
@@ -1,10 +1,17 @@
-fn hello_thread() {
-    print!("Hello from thread\n");
+fn hello_thread(n: i32) {
+    print!("Hello from thread {n}\n");
 }
 
 fn main() {
     print!("Hello from MAIN thread\n");
 
-    let thread_handle = std::thread::spawn(hello_thread);
-    thread_handle.join().expect("");
+    let mut thread_handles_vector = Vec::new();
+    for i in 0..25 {
+        let thread_handler = std::thread::spawn(move || hello_thread(i));
+        thread_handles_vector.push(thread_handler)
+    }
+
+    thread_handles_vector
+        .into_iter()
+        .for_each(|h| h.join().expect(""));
 }
