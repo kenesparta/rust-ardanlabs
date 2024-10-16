a684387e15fd318bb67a0dd0fcd763e5d551aad7
Ken Esparta <kenesparta@pm.me>
2024-10-16 11:43:13 -0500

    z2p: adding health check

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(_: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
