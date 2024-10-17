9eaaca3c6dcda13bc6fcce485c04e0992abb5a26
Ken Esparta <kenesparta@pm.me>
2024-10-17 00:28:41 -0500

    z2p: add new endpoint and unittests

--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -6,14 +8,14 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let addr = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", addr))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -23,8 +25,61 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 // Launch our application in the background ~somehow~
-fn spawn_app() {
-    let server = zero2prod::run().expect("failed executing zero2prod.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("failed executing zero2prod.");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
