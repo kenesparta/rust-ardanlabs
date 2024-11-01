a61d305b54d0eddbf1cc1f46206af9c4ab2bc55c
Ken Esparta <kenesparta@pm.me>
2024-11-01 16:36:56 -0500

    z2p: adding the pg connection from sqlx

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,13 +1,16 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(tcp_listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(tcp_listener)?
     .run();
