e7e36bf5f747907ae60a015fdf92cbd397e6a6db
Ken Esparta <kenesparta@pm.me>
2024-11-15 00:46:36 -0500

    z2p: new tracing

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -22,16 +23,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         subscriber_name = %form.name,
     );
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
+    // tracing::info!(
+    //     "request_id {} - Adding '{}' '{}' as a new subscriber.",
+    //     request_id,
+    //     form.email,
+    //     form.name
+    // );
+    // tracing::info!(
+    //     "request_id {} - Saving new subscriber details in the database",
+    //     request_id
+    // );
 
     match sqlx::query!(
         r#"
@@ -44,6 +48,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now(),
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
